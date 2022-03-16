fb4d56a1be70fa5ab9b5b64ade40f84e9f806f7f
Flookzaq1234 <darflook@gmail.com>
2022-03-16 09:00:33 +0700

    Update submarine.cpp

--- submarine.cpp
+++ submarine.cpp
@@ -26,12 +26,12 @@ int right_joystick_mapped_x_position = 0;
 int right_joystick_mapped_y_position = 0;
 
 // * motor ports
-#define left_motor_port1 1
+#define left_motor_port1 7
 #define left_motor_port2 2
-#define right_motor_port1 0
-#define right_motor_port2 0
-#define updown_motor_port1 0
-#define updown_motor_port2 0
+#define right_motor_port1 4
+#define right_motor_port2 3
+#define updown_motor_port1 8
+#define updown_motor_port2 9
 
 // * Controller plans
 // Left up => Left x positive
@@ -43,8 +43,6 @@ int right_joystick_mapped_y_position = 0;
 // Right down => Right x negative
 // Right left => Right y negative
 // Right right => Right y positive
-
-// When the sub needs to go forward
 
 void setup() {
     pinMode(left_joystick_x_pin, INPUT);
@@ -82,26 +80,88 @@ void loop() {
     right_joystick_mapped_x_position = map(right_joystick_x_position, 0, 1023, 512, -512);
     right_joystick_mapped_y_position = map(right_joystick_y_position, 0, 1023, 512, -512);
 
-    // * Capture any joystick movements if any
-    if (
-        left_joystick_mapped_x_position < negative_joystick_threshold || left_joystick_mapped_x_position > joystick_threshold ||
-        left_joystick_mapped_y_position < negative_joystick_threshold || left_joystick_mapped_y_position > joystick_threshold ||
-        right_joystick_mapped_x_position < negative_joystick_threshold || right_joystick_mapped_x_position > joystick_threshold ||
-        right_joystick_mapped_y_position < negative_joystick_threshold || right_joystick_mapped_y_position > joystick_threshold) {
-        if (left_joystick_mapped_x_position > joystick_threshold) {
-            Serial.println("going forward");
+    if (left_joystick_mapped_x_position < negative_joystick_threshold || left_joystick_mapped_x_position > joystick_threshold ||
+        left_joystick_mapped_y_position < negative_joystick_threshold || left_joystick_mapped_y_position > joystick_threshold) {
+        // * Capture all 8 different movements a left joystick could be. Starting from the bottom.
+        if (left_joystick_mapped_y_position < negative_joystick_threshold && left_joystick_mapped_x_position > joystick_threshold) {
+            digitalWrite(left_motor_port1, HIGH);
+            digitalWrite(left_motor_port2, LOW);
+            digitalWrite(right_motor_port1, HIGH);
+            digitalWrite(right_motor_port2, LOW);
+            // * joystick going left up
+            Serial.println("left going left up");
+        } else if (left_joystick_mapped_y_position > joystick_threshold && left_joystick_mapped_x_position > joystick_threshold) {
+            digitalWrite(left_motor_port1, LOW);
+            digitalWrite(left_motor_port2, HIGH);
+            digitalWrite(right_motor_port1, HIGH);
+            digitalWrite(right_motor_port2, LOW);
+            // * joystick going right up
+            Serial.println("left going right up");
+        } else if (left_joystick_mapped_y_position < negative_joystick_threshold && left_joystick_mapped_x_position < negative_joystick_threshold) {
+            digitalWrite(left_motor_port1, HIGH);
+            digitalWrite(left_motor_port2, LOW);
+            digitalWrite(right_motor_port1, LOW);
+            digitalWrite(right_motor_port2, HIGH);
+            // * joystick going left down
+            Serial.println("left going left down");
+        } else if (left_joystick_mapped_y_position > joystick_threshold && left_joystick_mapped_x_position < negative_joystick_threshold) {
+            digitalWrite(left_motor_port1, LOW);
+            digitalWrite(left_motor_port2, HIGH);
+            digitalWrite(right_motor_port1, LOW);
+            digitalWrite(right_motor_port2, HIGH);
+            // * joystick going right down
+            Serial.println("left going right down");
+        } else if (left_joystick_mapped_x_position > joystick_threshold) {
+            digitalWrite(left_motor_port1, HIGH);
+            digitalWrite(left_motor_port2, LOW);
+            digitalWrite(right_motor_port1, HIGH);
+            digitalWrite(right_motor_port2, LOW);
+            // * joystick going only up
+            Serial.println("left going only up");
         } else if (left_joystick_mapped_x_position < negative_joystick_threshold) {
-            Serial.println("going backward");
-        } else if (left_joystick_mapped_y_position > joystick_threshold) {
-            Serial.println("going right");
+            digitalWrite(left_motor_port1, LOW);
+            digitalWrite(left_motor_port2, HIGH);
+            digitalWrite(right_motor_port1, LOW);
+            digitalWrite(right_motor_port2, HIGH);
+            // * joystick going only down
+            Serial.println("left going only down");
         } else if (left_joystick_mapped_y_position < negative_joystick_threshold) {
-            Serial.println("going left");
-        } else if (right_joystick_mapped_x_position > joystick_threshold) {
-            Serial.println("going up");
-        } else if (right_joystick_mapped_x_position < negative_joystick_threshold) {
-            Serial.println("going down");
+            digitalWrite(left_motor_port1, HIGH);
+            digitalWrite(left_motor_port2, LOW);
+            digitalWrite(right_motor_port1, LOW);
+            digitalWrite(right_motor_port2, HIGH);
+            // * joystick going only left
+            Serial.println("left going only left");
+        } else if (left_joystick_mapped_y_position > joystick_threshold) {
+            digitalWrite(left_motor_port1, LOW);
+            digitalWrite(left_motor_port2, HIGH);
+            digitalWrite(right_motor_port1, HIGH);
+            digitalWrite(right_motor_port2, LOW);
+            // * joystick going only right
+            Serial.println("left going only right");
         }
     } else {
-        Serial.println("not working");
+        Serial.println("waiting for left joystick move");
+        digitalWrite(left_motor_port1, LOW);
+        digitalWrite(left_motor_port2, LOW);
+        digitalWrite(right_motor_port1, LOW);
+        digitalWrite(right_motor_port2, LOW);
+    }
+
+    if (right_joystick_mapped_x_position < negative_joystick_threshold || right_joystick_mapped_x_position > joystick_threshold ||
+        right_joystick_mapped_y_position < negative_joystick_threshold || right_joystick_mapped_y_position > joystick_threshold) {
+        if (right_joystick_mapped_x_position > joystick_threshold) {
+            digitalWrite(updown_motor_port1, HIGH);
+            digitalWrite(updown_motor_port2, LOW);
+            Serial.println("right going up");
+        } else if (right_joystick_mapped_x_position < negative_joystick_threshold) {
+            digitalWrite(updown_motor_port1, LOW);
+            digitalWrite(updown_motor_port2, HIGH);
+            Serial.println("right going down");
+        }
+    } else {
+        digitalWrite(updown_motor_port1, LOW);
+        digitalWrite(updown_motor_port2, LOW);
+        Serial.println("waiting for right joystick move");
     }
 }
